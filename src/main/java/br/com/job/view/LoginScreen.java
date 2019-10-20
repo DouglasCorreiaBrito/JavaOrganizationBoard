2c07e119ce008dc4a0cb92e21b6f75c19fe29fe0
Douglas <douglas.lusvardi@gmail.com>
2019-10-20 17:34:02 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/job/view/LoginScreen.java
@@ -0,0 +1,14 @@
+package br.com.job.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginScreen extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
