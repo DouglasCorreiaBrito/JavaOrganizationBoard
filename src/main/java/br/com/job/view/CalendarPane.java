784b2d30f1bb8a48dff337df92f7fd1aab1b4293
Douglas <douglas.lusvardi@gmail.com>
2019-11-13 15:41:09 -0300

    WIP - ScheduleBuilder feature

--- src/main/java/br/com/job/view/CalendarPane.java
+++ src/main/java/br/com/job/view/CalendarPane.java
@@ -1,15 +1,18 @@
 package br.com.job.view;
 
 import java.awt.Dimension;
-import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
 import br.com.job.control.MenuControl;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 import jfxtras.scene.control.CalendarPicker;
 
 public class CalendarPane extends FlowPane {
-	
-	
+
 	MenuControl controller;
 
 	public CalendarPane(MenuControl controller) {
@@ -19,23 +22,39 @@ public class CalendarPane extends FlowPane {
 		loadCalendar(cp);
 		initListeners(cp);
 		getChildren().add(cp);
-		
+
 	}
 
 	private void initListeners(CalendarPicker cp) {
-		
-		cp.setOnMousePressed(e-> callScheduleBuilder(cp));
+
+		cp.calendarProperty().addListener(new ChangeListener<Calendar>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Calendar> observable, Calendar oldValueDate, Calendar newValueDate) {
+				System.out.println(" Data selecionada: " + newValueDate.getTime().toString());
+				callScheduleBuilder(newValueDate.getTime());
+
+			}
+
+		});
 	}
 
-	private void callScheduleBuilder(CalendarPicker cp) {
-		System.out.println("teste");
+	private void callScheduleBuilder(Date newValueDate) {
+		try {
+			new ScheduleBuilder(newValueDate).start(new Stage());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private void loadCalendar(CalendarPicker cp) {
+		cp.withCalendar(Calendar.getInstance());
+		cp.withShowTime(Boolean.TRUE);
 		Dimension d = BaseStage.getScreenResolution();
-		double width = d.getWidth()*0.52;
-		double heigth = d.getHeight()*0.40;
+		double width = d.getWidth() * 0.52;
+		double heigth = d.getHeight() * 0.40;
 		cp.setMinSize(width, heigth);
 	}
-	
+
 }
