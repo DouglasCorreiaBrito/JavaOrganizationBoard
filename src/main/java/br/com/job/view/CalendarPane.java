98fe11acdea354c1063747ae8db865bacb02ddda
Douglas <douglas.lusvardi@gmail.com>
2019-11-23 15:31:31 -0300

    WIP - Schedule

--- src/main/java/br/com/job/view/CalendarPane.java
+++ src/main/java/br/com/job/view/CalendarPane.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import br.com.job.control.MenuControl;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import jfxtras.scene.control.CalendarPicker;
@@ -14,34 +15,33 @@ import jfxtras.scene.control.CalendarPicker;
 public class CalendarPane extends FlowPane {
 
 	MenuControl controller;
+	private CalendarPicker cp = new CalendarPicker();
+	private Button btnschedule = new Button("Agendar");
+	private Date dateToSchedule;
 
 	public CalendarPane(MenuControl controller) {
 		this.controller = controller;
 		setStyle("-fx-background-color: #00FF7F");
-		CalendarPicker cp = new CalendarPicker();
 		loadCalendar(cp);
-		initListeners(cp);
-		getChildren().add(cp);
-
+		initListeners(cp, btnschedule);
+		getChildren().addAll(cp, btnschedule);
 	}
 
-	private void initListeners(CalendarPicker cp) {
-
+	private void initListeners(CalendarPicker cp, Button btnschedule) {
+		
 		cp.calendarProperty().addListener(new ChangeListener<Calendar>() {
-
 			@Override
 			public void changed(ObservableValue<? extends Calendar> observable, Calendar oldValueDate, Calendar newValueDate) {
-				System.out.println(" Data selecionada: " + newValueDate.getTime().toString());
-				callScheduleBuilder(newValueDate.getTime());
-
+				dateToSchedule = newValueDate.getTime();
 			}
-
 		});
+
+		btnschedule.setOnAction(e -> callScheduleBuilder(dateToSchedule));
 	}
 
-	private void callScheduleBuilder(Date newValueDate) {
+	private void callScheduleBuilder(Date dateToSchedule) {
 		try {
-			new ScheduleBuilder(newValueDate).start(new Stage());
+			new ScheduleBuilder(dateToSchedule).start(new Stage());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/main/java/br/com/job/view/ScheduleBuilder.java
+++ src/main/java/br/com/job/view/ScheduleBuilder.java
@@ -1,75 +1,95 @@
 package br.com.job.view;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import br.com.job.model.Schedule;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.web.HTMLEditor;
 import javafx.stage.Stage;
 
 public class ScheduleBuilder extends Application {
 
-	private Schedule schedule;
 	private Date dateToSchedule;
-	private Label stringDateToSchedule;
+	private Label labelDateToSchedule;
 	private Label lbTitle;
 	private Label lbDesc;
 	private TextField tfTitle;
-	private HTMLEditor taDesc;
+	private TextArea taDesc;
 	private Button btnSave;
 	private Button btnClose;
 	private GridPane pane;
-	
+	private SimpleDateFormat sdf;
+	private static Stage stage;
+
 	public ScheduleBuilder(Date dateToSchedule) {
+		sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
 		this.dateToSchedule = dateToSchedule;
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		
+		ScheduleBuilder.stage = stage;
 		initComponents();
 		initLayout();
+		initListeners();
 		Scene scene = new Scene(pane);
 		stage.setResizable(false);
 		stage.setTitle("Agende seu compromisso");
 		stage.setScene(scene);
 		stage.show();
+
+	}
+
+	private void initListeners() {
 		
+		btnClose.setOnAction(e-> stage.close());
 	}
 
 	private void initLayout() {
-		
+
 		pane.add(lbTitle, 0, 0);
 		pane.add(tfTitle, 0, 1);
 		pane.add(lbDesc, 0, 2);
 		pane.add(taDesc, 0, 3);
-		pane.add(stringDateToSchedule, 0, 4);
+		pane.add(labelDateToSchedule, 0, 4);
 		pane.add(btnSave, 0, 5);
 		pane.add(btnClose, 0, 6);
-		
+
 	}
 
 	private void initComponents() {
 		lbTitle = new Label("Título da tarefa");
+		lbTitle.setStyle("-fx-font-size: 24px;" +
+            " -fx-text-fill: #FFF;" +
+            " -fx-font-family: 'Times New Roman';");
+		
 		lbDesc = new Label("Descreva seu agendamento");
-		taDesc = new HTMLEditor();
+		lbDesc.setStyle("-fx-font-size: 18px;" +
+            " -fx-text-fill: #FFF;" +
+            " -fx-font-family: 'Times New Roman';");
+		
+		taDesc = new TextArea();
 		tfTitle = new TextField();
-		stringDateToSchedule = new Label(dateToSchedule.toString());
+		labelDateToSchedule = new Label(" Você está agendando seu compromisso para: " + sdf.format(dateToSchedule));
+		labelDateToSchedule.setStyle("-fx-font-size: 18px;" +
+            " -fx-text-fill: #FFF;" +
+            " -fx-font-family: 'Times New Roman';");
 		btnClose = new Button("Cancelar");
 		btnSave = new Button("Salvar");
 		pane = new GridPane();
-		pane.setPrefSize(300, 600);
-		pane.setStyle("-fx-background-color : linear-gradient(#16EDEB,#175B60)");
+		pane.setPrefSize(600, 300);
+		pane.setStyle("-fx-background-color: #222222;"
+        		+ "-fx-border-color: white;"
+        		+ "-fx-border-width: 2;");
 		
-	}
 
+	}
 
 }
